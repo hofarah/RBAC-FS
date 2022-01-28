4ca221708e7fd501308ef54392fc5953424de5b2
hossein <hoseyn.farahmandnezhad@gmail.com>
2022-01-28 17:53:16 +0330

    add command handler

--- internal/command.go
+++ internal/command.go
@@ -1,15 +1,46 @@
 package internal
 
-type command struct {
+import (
+	"github.com/hofarah/RBAC-FS/utils"
+	"strings"
+)
+
+type Command struct {
 	c    string
 	args []string
 }
 
-func (c *command) Do() string {
-	//todo run command
-	return ""
+const (
+	ListCMD         = "list"
+	OpenCMD         = "open"
+	BackCMD         = "back"
+	ReadCMD         = "read"
+	HelpCMD         = "help"
+	OpenUsageString = "wrong COMMAND. usage: " + OpenCMD + " <folder name>"
+	ReadUsageString = "wrong COMMAND. usage: " + ReadCMD + " <file name>"
+	HelpString      = "COMMANDS\n" + ListCMD + " - list of current directory\n" + BackCMD + " - go to parent directory\n" + OpenCMD + " - open folder\n" + ReadCMD + " - open file"
+	colorReset      = "\033[0m"
+
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
+	colorWhite  = "\033[37m"
+)
+
+var validCommands = []string{ListCMD, OpenCMD, BackCMD, ReadCMD, HelpCMD}
+
+func (c *Command) Validate() bool {
+	return utils.Contain(validCommands, c.c)
 }
 
-type Parser interface {
-	Parse() string
+func (c *Command) GetType() string {
+	return c.c
+}
+func NewCommand(line string) Command {
+	line = strings.TrimSpace(line)
+	args := strings.Split(line, " ")
+	return Command{c: args[0], args: args[1:]}
 }
