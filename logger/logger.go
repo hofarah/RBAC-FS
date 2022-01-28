9cc03c70a7d7a34316a7a9b3fb2d93bf21d87833
hossein <hoseyn.farahmandnezhad@gmail.com>
2022-01-28 22:39:21 +0330

    logger added

--- logger/logger.go
+++ logger/logger.go
@@ -3,15 +3,20 @@ package logger
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
 )
 
 func init() {
-
+	logAddr := "/tmp/app.log"
+	_, err := os.ReadFile(logAddr)
+	if err != nil {
+		os.Create(logAddr)
+	}
 	var level = zapcore.DebugLevel
 	logger, _ := zap.Config{
 		Encoding:    "json",
 		Level:       zap.NewAtomicLevelAt(level),
-		OutputPaths: []string{"./logs/app.log"},
+		OutputPaths: []string{"/tmp/app.log"},
 		EncoderConfig: zapcore.EncoderConfig{
 			MessageKey:   "message",
 			LevelKey:     "level",
